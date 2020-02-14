df1ed95e5c1a162229e280c4dd58a46f89713de1
Ronnie Rodriguez <rlrodrig88@gmail>
2020-02-13 22:02:29 -0600

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -30,14 +31,37 @@ class About extends Component {
 
   render() {
     const renderPartner = ({item}) => {
-      return (
-          <ListItem
-              title={item.name}
-              subtitle={item.description}
-              leftAvatar={{source: {uri: baseUrl + item.image}}}
-          />
-      );
-  };
+        return (
+            <ListItem
+                title={item.name}
+                subtitle={item.description}
+                leftAvatar={{source: {uri: baseUrl + item.image}}}
+            />
+        );
+    };
+
+    if (this.props.partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if (this.props.partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Text>{this.props.partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
 
     return(
       <ScrollView>
