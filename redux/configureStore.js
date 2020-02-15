4eb9b0b6d1eef9b16e44d3fc5f84b4030c696036
Ronnie Rodriguez <rlrodrig88@gmail>
2020-02-14 21:52:47 -0600

    Redux Adding Favorites

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -5,16 +5,18 @@ import { campsites } from './campsites';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
+import { favorites } from './favorites';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
-            campsites,
-            comments,
-            partners,
-            promotions
-        }),
-        applyMiddleware(thunk, logger)
+      combineReducers({
+        campsites,
+        comments,
+        partners,
+        promotions,
+        favorites
+    }),
+      applyMiddleware(thunk, logger)
     );
 
     return store;
