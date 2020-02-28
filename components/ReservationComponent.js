8b4435275399bf9d966ffa573a5c0e58852eecd7
Ronnie Rodriguez <rlrodrig88@gmail>
2020-02-27 22:25:35 -0600

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -39,7 +41,10 @@ class Reservation extends Component {
           },
           {
             text: 'OK',
-            onPress: () => this.resetForm()
+            onPress: () => {
+              this.presentLocalNotification(this.state.date);
+              this.resetForm();
+          }
           }
         ],
         // options
@@ -47,6 +52,28 @@ class Reservation extends Component {
       );   
     }
 
+    async obtainNotificationPermission() {
+      const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+      if (permission.status !== 'granted') {
+          const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+          if (permission.status !== 'granted') {
+              Alert.alert('Permission not granted to show notifications');
+          }
+          return permission;
+      }
+      return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
+    }
+        
     resetForm() {
         this.setState({
             campers: 1,
